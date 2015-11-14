641c1082ea89497f3aa34a62e5bbf5eb1e6dc508
Demonsheep <demonsheep@fromhell.org>
2015-11-13 19:44:43 -0600

    Handle more ustream BS.

--- plugins/hhtwitter.rb
+++ plugins/hhtwitter.rb
@@ -10,19 +10,29 @@
 require 'open-uri'
 require 'pp'
 require 'tweetstream'
+require 'uri'
 
 class Cinch::Plugins::HHTwitter
   include Cinch::Plugin
 
   @hh_twitter
+  @last_tweet
 
-  match /hhtweets (on|off)\s*$/,    :method => :set_hh_tweets
+  match /hughtweets (on|off)\s*$/,  :method => :set_hugh_tweets
   listen_to :connect,               :method => :on_connect
 
   def initialize(*args)
     super
 
     @hh_twitter = false
+    @last_tweet = Time.new.to_i
+
+    # stupid ustream. flood limit is 3 lines every 5 seconds
+    # so a multi-line tweet or a tweet with more than 3 lines
+    # in 5 seconds gets us kicked out.
+    @line_limit = 3 # lines
+    @time_limit = 8 # seconds
+
   end
 
 
@@ -39,7 +49,7 @@ class Cinch::Plugins::HHTwitter
 
   end
 
-  def set_hh_tweets(msg, state)
+  def set_hugh_tweets(msg, state)
 
     state = state == 'on' ? true : false
 
@@ -49,44 +59,94 @@ class Cinch::Plugins::HHTwitter
       return
     end
 
-    state ? toggle_hh_tweets_on(msg) : toggle_hh_tweets_off(msg)
+    state ? toggle_hugh_tweets_on(msg) : toggle_hugh_tweets_off(msg)
 
   end
 
-  def toggle_hh_tweets_on(msg)
+  def toggle_hugh_tweets_on(msg)
     @hh_twitter = true
 
-    msg.reply("watching tweets")
+    msg.reply('ok')
 
     client = TweetStream::Client.new
 
     client.on_error do |message|
-      puts "error: #{message}"
-      msg.reply "Got an error, stopping tweet stream (#{message})"
+      msg.reply "Got an error, stopping tweet stream (#{message.lines.first})"
       @hh_twitter = false
       client.stop
     end
 
     client.follow(15075999) do |status, client|
-      if @hh_twitter === false
-        msg.reply 'stopping'
-        client.stop
+
+      client.stop unless @hh_twitter
+
+      # skip this tweet if it doesn't meet the criteria
+      next unless repeatable?(status)
+
+      if is_retweet?(status)
+        text = original_text_of_rt(status)
+      else
+        text = status.text
       end
 
-      # Only show tweets from Hugh
-      if status.attrs[:user][:id] == 15075999 && status.in_reply_to_status_id.nil?
-        msg.reply "@HughHewitt: #{status.text}"
-      end
+      # stupid ustream considers most public url shorteners as spam
+      # so we'll wrap up the url in a dfm shortened url.
+      text = wrap_urls(text)
+
+      msg.reply "@HughHewitt: #{text}"
+      @last_tweet = Time.new.to_i
 
     end
-
   end
 
-  def toggle_hh_tweets_off(msg)
+  def toggle_hugh_tweets_off(msg)
     @hh_twitter = false
+    msg.reply('ok - no more HughTweets')
     return
   end
 
+  def original_text_of_rt(status)
+    return status.attrs['retweeted_status']['text']
+  end
+
+  def is_retweet?(status)
+    status.attrs.has_key?('retweeted_status')
+  end
+
+  def wrap_urls(text)
+    new_text = text.clone
+    URI.extract(text) do |url|
+      new_text = new_text.sub(url,shorten_url(url))
+    end
+
+    return new_text
+  end
+
+  def shorten_url(url)
+
+    begin
+      short_url = open("http://duanefm.com/l/shorten.php?longurl=#{URI.escape(url)}").read
+      short_url == 'Error' ? url : short_url
+    rescue OpenURI::HTTPError
+      url
+    end
+
+    short_url
+  end
+
+  def repeatable?(status)
+
+    repeatable = true
+
+    repeatable = false unless status.attrs[:user][:id] == 15075999
+    repeatable = false unless status.in_reply_to_status_id.nil?
+    repeatable = false unless status.text.lines.count <= @line_limit
+    repeatable = false unless Time.new.to_i > (@last_tweet + @time_limit)
+
+    pp status.attrs if repeatable
+
+    repeatable
+  end
 
 end
 
